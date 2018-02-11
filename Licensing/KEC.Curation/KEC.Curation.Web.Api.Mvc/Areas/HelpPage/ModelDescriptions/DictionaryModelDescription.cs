619d4357d776240029fb3871218f42761a0c85dc
Collins Omwenga <comwenga@live.com>
2018-02-11 14:11:01 +0000

    Merged PR 33: Merge Curation to development

--- Licensing/KEC.Curation/KEC.Curation.Web.Api.Mvc/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace KEC.Curation.Web.Api.Mvc.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- Licensing/KEC.Curation/KEC.Curation.Web.Api.Mvc/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace KEC.Curation.Web.Api.Mvc.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
