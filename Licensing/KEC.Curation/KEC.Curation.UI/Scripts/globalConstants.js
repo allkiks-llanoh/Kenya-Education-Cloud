4aff060ccb2b7ec37064e159dd6855e7c9c0c6cd
allkiks <allan.kkoech@gmail.com>
2018-02-21 21:11:16 +0300

    baseUralActivation

--- Licensing/KEC.Curation/KEC.Curation.UI/Scripts/globalConstants.js
+++ Licensing/KEC.Curation/KEC.Curation.UI/Scripts/globalConstants.js
@@ -1,4 +1,4 @@
-﻿//const apiBaseUrl = "https://curationapi-d.kec.ac.ke/api";
+﻿const apiBaseUrl = "https://curationapi-d.kec.ac.ke/api";
 function ShowAlert(message, type) {
     $(".alert").addClass('hidden');
     if (type === "error") {
