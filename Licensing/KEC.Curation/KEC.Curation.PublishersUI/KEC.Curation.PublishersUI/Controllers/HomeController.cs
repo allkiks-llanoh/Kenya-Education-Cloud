2d3514d0466c2a86ab4c52f6d6a283b5c1f88816
allkiks <allan.kkoech@gmail.com>
2018-02-24 12:03:21 +0300

    added datatype

---
+++ Licensing/KEC.Curation/KEC.Curation.PublishersUI/KEC.Curation.PublishersUI/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using KEC.Curation.PublishersUI.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KEC.Curation.PublishersUI.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+
+        public ActionResult About()
+        {
+           
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Licensing/KEC.Curation/KEC.Curation.PublishersUI/KEC.Curation.PublishersUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KEC.Curation.PublishersUI.MvcApplication" Language="C#" %>
