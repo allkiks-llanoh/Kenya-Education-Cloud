22c0554356bf3d2e786bcf4ff02ded222b27ec06
allkiks <allan.kkoech@gmail.com>
2018-02-26 21:55:09 +0300

    Defined Url

--- Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/js/GetChiefCurators.js
+++ Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/js/GetChiefCurators.js
@@ -1,35 +1,7 @@
 ﻿
-//$select = $('#UserGuid');
-////request the JSON data and parse into the select element
-//$.ajax({
-//    headers: {
-//        'Accept': 'application / json',
-//        'Content-type': 'application/json'
-//    },
-   
-//    type: 'GET',
-//    url: "https://localhost:44317/userprofile/GetTokenForApplication",
-//    dataType: 'json',
-//    async: false,
-//    crossDomain: true,
-   
-//    success: function (data) {
-//        //clear the current content of the sel
-//        $select.html('');
-//        //iterate over the data and append a select option
-//        $.each(data, function (index, val) {
-//            $select.append(`<option id=${val[0].word}  value='${val[0].word}'+ >${val.givenName}</option>`);
-//        })
-//    },
-//    error: function () {
-//        //if there is an error append a 'none available' option
-//        $select.html('<option id="-1">none available</option>');
-//    }
-//});
-
 $(function () {
 
-    AjaxCall('https://localhost:44317/userprofile/GetTokenForApplication', null).done(function (response) {
+    AjaxCall('https://curation-d/api/userprofile/GetTokenForApplication', null).done(function (response) {
         if (response.length > 0) {
 
             $('#UserGuid').html('');
