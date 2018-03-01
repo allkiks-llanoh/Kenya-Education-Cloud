fb601f1ac06dc7740b3fc5109bf331f7d061e0ef
Daniel Juma <juma@miyabiafrica.com>
2018-03-01 23:24:40 +0300

    Used call back for setting user guid

--- Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/js/globalConstants.js
+++ Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/js/globalConstants.js
@@ -1,19 +1,37 @@
 ﻿const apiBaseUrl = "https://curationapi-d/api";
-var userGuid;
+var currentUserGuid;
 $(document).ready(function () {
 
     $('.summernote').summernote();
-    currentUserGuid = readHeader('x-user-guid');
-    console.log(userGuid);
+    fetchUserHeader((userGuid) => {
+        currentUserGuid = userGuid;
+        console.log(currentUserGuid);
+    })
    
 });
 
-function readHeader(name) {
-    let req = new XMLHttpRequest();
-    req.open('GET', document.location, false);
-    req.send(null);
-    let header = req.getResponseHeader(name);
-    return header;
+function fetchUserHeader(callback) {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === 4) {
+            //
+            // The following headers may often be similar
+            // to those of the original page request...
+            //
+            if (callback && typeof callback === 'function') {
+                callback(request.getResponseHeader('x-user-guid'));
+            }
+        }
+    };
+
+    //
+    // Re-request the same page (document.location)
+    // We hope to get the same or similar response headers to those which 
+    // came with the current page, but we have no guarantee.
+    // Since we are only after the headers, a HEAD request may be sufficient.
+    //
+    request.open('HEAD', document.location, true);
+    request.send(null);
 }
 
 function ShowAlert(message, type) {
