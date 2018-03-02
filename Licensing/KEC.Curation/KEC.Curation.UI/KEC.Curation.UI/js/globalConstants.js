96c02427a7b570232fdfa7a56e2972bb223833a8
Daniel Juma <juma@miyabiafrica.com>
2018-03-02 20:13:25 +0300

    Chief curator refactoring

--- Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/js/globalConstants.js
+++ Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/js/globalConstants.js
@@ -5,8 +5,7 @@ $(document).ready(function () {
     $('.summernote').summernote();
     fetchUserHeader((userGuid) => {
         currentUserGuid = userGuid;
-        console.log(currentUserGuid);
-    })
+    });
    
 });
 
