b97abe12d9ecb450133dbe4912574547c788a16e
Daniel Juma <juma@miyabiafrica.com>
2018-03-02 19:37:36 +0300

    Fetched user from HttpContext

--- Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/ActionFilters/UserGuidJsonAttribute .cs
+++ Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/ActionFilters/UserGuidJsonAttribute .cs
@@ -14,8 +14,16 @@ namespace KEC.Curation.UI.ActionFilters
         }
         public void SetUserGuid(ActionExecutingContext filterContext)
         {
-            var signedInUserID = ClaimsPrincipal.Current.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            filterContext.RequestContext.HttpContext.Response.Headers.Set("X-User-Guid", signedInUserID ?? string.Empty);
+            if (filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                var user = filterContext.HttpContext.User.Identity as ClaimsIdentity;
+                var signedInUserID = user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                filterContext.RequestContext.HttpContext.Response.AddHeader("X-User-Guid", signedInUserID ?? string.Empty);
+            }
+            else
+            {
+                filterContext.HttpContext.Response.Headers.Remove("X-User-Guid");
+            }
             
         }
     }
