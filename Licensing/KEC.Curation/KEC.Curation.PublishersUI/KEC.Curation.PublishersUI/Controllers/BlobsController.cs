d9e07ccfcf34b41465edf920225cf4d1479a7d80
Collins Omwenga <comwenga@live.com>
2018-03-04 01:28:25 +0300

    Added blob storage manipulation code

---
+++ Licensing/KEC.Curation/KEC.Curation.PublishersUI/KEC.Curation.PublishersUI/Controllers/BlobsController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace KEC.Curation.PublishersUI.Controllers
+{
+    public class BlobsController : Controller
+    {
+        // GET: Blobs
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private CloudBlobContainer GetCloudBlobContainer()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+            CloudConfigurationManager.GetSetting("keccuration_AzureStorageConnectionString"));
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("publications");
+            return container;
+        }
+
+        public ActionResult CreateBlobContainer()
+        {
+            CloudBlobContainer container = GetCloudBlobContainer();
+            ViewBag.Success = container.CreateIfNotExists();
+            ViewBag.BlobContainerName = container.Name;
+
+            return View();
+        }
+
+        public string UploadBlob()
+        {
+            CloudBlobContainer container = GetCloudBlobContainer();
+            CloudBlockBlob blob = container.GetBlockBlobReference("header_one.jpg");
+            using (var fileStream = System.IO.File.OpenRead(@"C:\Users\collo\Source\Repos\Kenya Education Cloud\Licensing\KEC.Curation\KEC.Curation.PublishersUI\KEC.Curation.PublishersUI\Content\landing\header_one.jpg"))
+            {
+                blob.UploadFromStream(fileStream);
+            }
+            return "success!";
+        }
+
+        public ActionResult ListBlobs()
+        {
+            CloudBlobContainer container = GetCloudBlobContainer();
+
+            List<string> blobs = new List<string>();
+            foreach (IListBlobItem item in container.ListBlobs())
+            {
+                if (item.GetType() == typeof(CloudBlockBlob))
+                {
+                    CloudBlockBlob blob = (CloudBlockBlob)item;
+                    blobs.Add(blob.Name);
+                }
+                else if (item.GetType() == typeof(CloudPageBlob))
+                {
+                    CloudPageBlob blob = (CloudPageBlob)item;
+                    blobs.Add(blob.Name);
+                }
+                else if (item.GetType() == typeof(CloudBlobDirectory))
+                {
+                    CloudBlobDirectory dir = (CloudBlobDirectory)item;
+                    blobs.Add(dir.Uri.ToString());
+                }
+            }
+
+            return View(blobs);
+        }
+
+        public string DownloadBlob()
+        {
+            CloudBlobContainer container = GetCloudBlobContainer();
+            CloudBlockBlob blob = container.GetBlockBlobReference("header_one.jpg");
+            using (var fileStream = System.IO.File.OpenWrite(@"c:\header_one.jpg"))
+            {
+                blob.DownloadToStream(fileStream);
+            }
+            return "success!";
+        }
+    }
+}
---
+++ Licensing/KEC.Curation/KEC.Curation.PublishersUI/KEC.Curation.PublishersUI/Views/Blobs/BlobJS.js
@@ -0,0 +1,17 @@
+﻿
+function uploadBlobFromText() {
+     // your account and SAS information
+     var sasKey ="https://keccuration.blob.core.windows.net/?sv=2017-07-29&ss=bfqt&srt=sco&sp=rwdlacup&se=2019-01-05T06:25:18Z&st=2018-03-03T22:25:18Z&spr=https,http&sig=C4bE23Bha53TfRsTmnGHz1Y%2FeZ8v%2BcnhKwj5YSZDosQ%3D";
+     var blobUri = "http://keccuration.blob.core.windows.net";
+     var blobService = AzureStorage.createBlobServiceWithSas(blobUri, sasKey).withFilter(new AzureStorage.ExponentialRetryPolicyFilter());
+     var text = document.getElementById('text');
+     var btn = document.getElementById("upload-button");
+     blobService.createBlockBlobFromText('publications', 'myblob', text.value,  function(error, result, response){
+         if (error) {
+             alert('Upload filed, open browser console for more detailed info.');
+             console.log(error);
+         } else {
+             alert('Upload successfully!');
+         }
+     });
+}
