b2c20dbd053017eb79abc0a975032784f0400c54
Collins Omwenga <comwenga@live.com>
2018-03-04 01:32:30 +0300

    Aded the reference link to download the Azure Storage JavaScript Client Library for Browsers

--- Licensing/KEC.Curation/KEC.Curation.PublishersUI/KEC.Curation.PublishersUI/Views/Blobs/BlobJS.js
+++ Licensing/KEC.Curation/KEC.Curation.PublishersUI/KEC.Curation.PublishersUI/Views/Blobs/BlobJS.js
@@ -1,6 +1,8 @@
 ﻿
 function uploadBlobFromText() {
      // your account and SAS information
+
+    // get the Azure Storage JavaScript Client Library for Browsers from https://aka.ms/downloadazurestoragejs
      var sasKey ="https://keccuration.blob.core.windows.net/?sv=2017-07-29&ss=bfqt&srt=sco&sp=rwdlacup&se=2019-01-05T06:25:18Z&st=2018-03-03T22:25:18Z&spr=https,http&sig=C4bE23Bha53TfRsTmnGHz1Y%2FeZ8v%2BcnhKwj5YSZDosQ%3D";
      var blobUri = "http://keccuration.blob.core.windows.net";
      var blobService = AzureStorage.createBlobServiceWithSas(blobUri, sasKey).withFilter(new AzureStorage.ExponentialRetryPolicyFilter());
