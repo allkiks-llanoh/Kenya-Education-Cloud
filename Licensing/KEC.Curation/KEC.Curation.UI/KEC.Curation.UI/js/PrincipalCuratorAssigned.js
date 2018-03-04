dd0713ae61d9f312ac0b4e234e1fca1c8cc977fa
allkiks <allan.kkoech@gmail.com>
2018-03-04 13:59:43 +0300

    Scripts Seperations

--- Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/js/PrincipalCuratorAssigned.js
+++ Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/js/PrincipalCuratorAssigned.js
@@ -1,7 +1,7 @@
 ﻿
 
-let principalCuratorGetUrl = apiBaseUrl.concat(`/PrincipalCurator/PrincipalCurator`)
-var prGuid = $('#dataGUID').attr('data-pGUID');
+let principalCuratorAssignedUrl = apiBaseUrl.concat(`/PrincipalCurator/Curation`)
+//var prGuid = $('#dataGUID').attr('data-pGUID');
 
     function tableRows(data) {
         var tableRows = [];
@@ -11,16 +11,18 @@ var prGuid = $('#dataGUID').attr('data-pGUID');
         return tableRows;
     };
 
-    //Start by getting voucher list based on batch Id
+    //Assigned Table starts here
+
+    //Start by getting a list of contents that have been assigned to chief curators
     $.ajax({
-        url: principalCuratorGetUrl,
+        url: principalCuratorAssignedUrl,
         type: "GET",
         dataType: 'json',
         success: function (data, status, jqhxr) {
             console.log(data);
 
             //This code snipet prepares to append Json Data
-            $('#unassigned-publications').append(tableRows(data));
+            $('#assigned-publications').append(tableRows(data));
         }
     });
 
@@ -31,7 +33,5 @@ var prGuid = $('#dataGUID').attr('data-pGUID');
         row.append($("<td>" + rowData.title + "</td>"));
         row.append($("<td>" + rowData.description + "</td>"));
         row.append($("<td>" + rowData.kicdNumber + "</td>"));
-        row.append($(`<td> <a href="/PrincipalCurator/PrincipalCuratorReview/?Title=${rowData.title}&PGUID=${prGuid}&Identity=${rowData.id}&KICDN=${rowData.kicdNumber}&Publication=${rowData.url}&Stage=PrincipalCurator" class="btn btn-w-m btn-info" style="background-color:#00B95F;" role="button">Review</a>`));
-
         return row[0];
     } 
--- Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/js/PrincipalCuratorView.js
+++ Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/js/PrincipalCuratorView.js
@@ -1,7 +1,7 @@
 ﻿
 
 let principalCuratorGetUrl = apiBaseUrl.concat(`/PrincipalCurator/PrincipalCurator`)
-let principalCuratorAssignedUrl = apiBaseUrl.concat(`/PrincipalCurator/Curation`)
+
 var prGuid = $('#dataGUID').attr('data-pGUID');
 
    //Definition of global draw rows function
@@ -41,29 +41,5 @@ var prGuid = $('#dataGUID').attr('data-pGUID');
     } 
 
 
-    //Assigned Table starts here
-
-    //Start by getting a list of contents that have been assigned to chief curators
-    $.ajax({
-        url: principalCuratorAssignedUrl,
-        type: "GET",
-        dataType: 'json',
-        success: function (data, status, jqhxr) {
-            console.log(data);
-
-            //This code snipet prepares to append Json Data
-            $('#assigned-publications').append(tableRows(data));
-        }
-    });
-
-    //This functionpopulates the tbody inner HTML with json data on call
-    function drawRow(rowData) {
-        var row = $("<tr />")
-        row.append($("<td>" + rowData.id + "</td>"));
-        row.append($("<td>" + rowData.title + "</td>"));
-        row.append($("<td>" + rowData.description + "</td>"));
-        row.append($("<td>" + rowData.kicdNumber + "</td>"));
-        return row[0];
-    } 
-
+   
  
