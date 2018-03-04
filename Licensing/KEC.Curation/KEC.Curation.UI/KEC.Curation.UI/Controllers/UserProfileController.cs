6fd9d567963f3f96d24cdc38fa1a6da935f2b71d
allkiks <allan.kkoech@gmail.com>
2018-03-04 17:26:27 +0300

    Added Get Users

--- Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/Controllers/UserProfileController.cs
+++ Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/Controllers/UserProfileController.cs
@@ -17,6 +17,7 @@ using System.Net.Http.Headers;
 using Newtonsoft.Json;
 using Microsoft.Azure.ActiveDirectory.GraphClient.Extensions;
 using KEC.Curation.UI.ActionFilters;
+using System.Web.Script.Serialization;
 
 namespace KEC.Curation.UI.Controllers
 {
@@ -106,7 +107,7 @@ namespace KEC.Curation.UI.Controllers
 
         public async Task<string> GetTokenForApplication()
         {
-           
+
             string signedInUserID = ClaimsPrincipal.Current.FindFirst(ClaimTypes.NameIdentifier).Value;
             string tenantID = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid").Value;
             string userObjectID = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier").Value;
@@ -117,15 +118,36 @@ namespace KEC.Curation.UI.Controllers
             AuthenticationContext authenticationContext = new AuthenticationContext(aadInstance + tenantID, new ADALTokenCache(signedInUserID));
             AuthenticationResult authenticationResult = await authenticationContext.AcquireTokenSilentAsync(graphResourceID, clientcred, new UserIdentifier(userObjectID, UserIdentifierType.UniqueId));
 
-            //HttpClient client = new HttpClient();
-            //HttpRequestMessage request = new HttpRequestMessage(
-            //HttpMethod.Get, graphResourceID);
-            //request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", authenticationResult.AccessToken);
-            //var response = await client.SendAsync(request);
-
-            //var result = response.Content.ReadAsStringAsync().Result;
-            return (authenticationResult.AccessToken);
+            HttpClient client = new HttpClient();
+            HttpRequestMessage request = new HttpRequestMessage(
+            HttpMethod.Get, graphResourceID);
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", authenticationResult.AccessToken);
+            var response = await client.SendAsync(request);
+            var result = response.Content.ReadAsStringAsync().Result;
+            return (result);
         }
-       
+        [HttpGet]
+        public async Task<List<ActiveDirectoryUser>> GetUsers()
+        {
+
+            string signedInUserID = ClaimsPrincipal.Current.FindFirst(ClaimTypes.NameIdentifier).Value;
+            string tenantID = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid").Value;
+            string userObjectID = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/objectidentifier").Value;
+
+            // get a token for the Graph without triggering any user interaction (from the cache, via multi-resource refresh token, etc)
+            ClientCredential clientcred = new ClientCredential(clientId, appKey);
+            // initialize AuthenticationContext with the token cache of the currently signed in user, as kept in the app's database
+            AuthenticationContext authenticationContext = new AuthenticationContext(aadInstance + tenantID, new ADALTokenCache(signedInUserID));
+            AuthenticationResult authenticationResult = await authenticationContext.AcquireTokenSilentAsync(graphResourceID, clientcred, new UserIdentifier(userObjectID, UserIdentifierType.UniqueId));
+
+            HttpClient client = new HttpClient();
+            HttpRequestMessage request = new HttpRequestMessage(
+            HttpMethod.Get, graphResourceIDGroups);
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", authenticationResult.AccessToken);
+            var response = await client.SendAsync(request);
+            var result = response.Content.ReadAsStringAsync().Result;
+            var users = (new JavaScriptSerializer()).Deserialize<List<ActiveDirectoryUser>>(result);
+            return users;
+        }
     }
 }
