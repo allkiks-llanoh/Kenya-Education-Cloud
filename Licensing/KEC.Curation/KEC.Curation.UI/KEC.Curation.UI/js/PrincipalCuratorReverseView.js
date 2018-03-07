3dfee05658f2c3bc17f67f8072aa826f1a2a550f
allkiks <allan.kkoech@gmail.com>
2018-03-07 17:40:10 +0300

    UI GET

--- Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/js/PrincipalCuratorReverseView.js
+++ Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/js/PrincipalCuratorReverseView.js
@@ -30,9 +30,11 @@ let principalCuratorAssignedUrl = apiBaseUrl.concat(`/PrincipalCurator/Curation`
     //This functionpopulates the tbody inner HTML with json data on call
     function drawRow(rowData) {
         var row = $("<tr />")
-     
-        row.append($("<td>" + rowData.description  + "</td>"));
+        row.append($("<td>" + rowData.id + "</td>"));
+        row.append($("<td>" + rowData.title + "</td>"));
+        row.append($("<td>" + rowData.description + "</td>"));
         row.append($("<td>" + rowData.kicdNumber + "</td>"));
-        row.append($("<td>" + rowData.kicdNumber + "</td>"));
+        row.append($(`<td> <a href="/PrincipalCurator/PrincipalCuratorReview/?Title=${rowData.title}&PGUID=${prGuid}&Identity=${rowData.id}&KICDN=${rowData.kicdNumber}&Publication=${rowData.url}&Stage=PrincipalCurator" class="btn btn-w-m btn-info" style="background-color:#00B95F;" role="button">Review</a>`));
+
         return row[0];
     } 
