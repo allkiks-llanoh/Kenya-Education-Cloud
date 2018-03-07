96053903a0af36b8051cdae5238f76ef29c8e98c
allkiks <allan.kkoech@gmail.com>
2018-03-07 18:23:41 +0300

    UI GET

--- Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/js/PrincipalCuratorAssigned.js
+++ Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/js/PrincipalCuratorAssigned.js
@@ -23,6 +23,7 @@ let principalCuratorAssignedUrl = apiBaseUrl.concat(`/PrincipalCurator/Curation`
 
             //This code snipet prepares to append Json Data
             $('#assigned-publications').append(tableRows(data));
+            $('#history-publications').append(tableRows(data));
             
         }
     });
@@ -30,9 +31,11 @@ let principalCuratorAssignedUrl = apiBaseUrl.concat(`/PrincipalCurator/Curation`
     //This functionpopulates the tbody inner HTML with json data on call
     function drawRow(rowData) {
         var row = $("<tr />")
-     
+        row.append($("<td>" + rowData.id + "</td>"));
+        row.append($("<td>" + rowData.title + "</td>"));
         row.append($("<td>" + rowData.description + "</td>"));
         row.append($("<td>" + rowData.kicdNumber + "</td>"));
-        row.append($("<td>" + rowData.kicdNumber + "</td>"));
+        row.append($(`<td> <a href="/PrincipalCurator/PrincipalCuratorReview/?Title=${rowData.title}&PGUID=${prGuid}&Identity=${rowData.id}&KICDN=${rowData.kicdNumber}&Publication=${rowData.url}&Stage=PrincipalCurator" class="btn btn-w-m btn-info" style="background-color:#00B95F;" role="button">Review</a>`));
+
         return row[0];
     } 
