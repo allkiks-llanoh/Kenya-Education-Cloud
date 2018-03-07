49c4a6288f50ade98b38d0a39f2f164fd056fd6a
allkiks <allan.kkoech@gmail.com>
2018-03-07 18:54:01 +0300

    Kupambana na json

--- Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/js/PrincipalCuratorReverseView.js
+++ Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/js/PrincipalCuratorReverseView.js
@@ -37,38 +37,4 @@ let principalCuratorAssignedUrl = apiBaseUrl.concat(`/PrincipalCurator/Curation`
        
         return row[0];
     } 
-    function tableRowss(data) {
-        var tableRows = [];
-        for (var i = 0; i < data.length; i++) {
-            tableRows.push(drawRows(data[i]));
-        }
-        return tableRows;
-    };
-
-    //Assigned Table starts here
-
-    //Start by getting a list of contents that have been assigned to chief curators
-    $.ajax({
-        url: principalCuratorAssignedUrl,
-        type: "GET",
-        dataType: 'json',
-        success: function (data, status, jqhxr) {
-            console.log(data);
-
-            //This code snipet prepares to append Json Data
-
-            $('#history-publications').append(tableRowss(data));
-        }
-    });
-
-    //This functionpopulates the tbody inner HTML with json data on call
-    function drawRows(rowData) {
-        var row = $("<tr />")
-        row.append($("<td>" + rowData.id + "</td>"));
-        row.append($("<td>" + rowData.title + "</td>"));
-        row.append($("<td>" + rowData.description + "</td>"));
-        row.append($("<td>" + rowData.kicdNumber + "</td>"));
-        row.append($(`<td> <a href="/PrincipalCurator/PrincipalCuratorReview/?Title=${rowData.title}&PGUID=${prGuid}&Identity=${rowData.id}&KICDN=${rowData.kicdNumber}&Publication=${rowData.url}&Stage=PrincipalCurator" class="btn btn-w-m btn-info" style="background-color:#00B95F;" role="button">Review</a>`));
-
-        return row[0];
-    } 
+    
