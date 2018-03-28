c1aa46921937886d9d2092ae602b746a403bcc2a
Collins Omwenga <comwenga@live.com>
2018-03-28 14:30:02 +0300

    Identity

--- Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/js/PrincipalCuratorView.js
+++ Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/js/PrincipalCuratorView.js
@@ -36,7 +36,7 @@ let principalCuratorGetUrl = apiBaseUrl.concat(`/PrincipalCurator/PrincipalCurat
         row.append($("<td>" + rowData.title + "</td>"));
         row.append($("<td>" + rowData.description + "</td>"));
         row.append($("<td>" + rowData.kicdNumber + "</td>"));
-        row.append($(`<td> <a href="/PrincipalCurator/PrincipalCuratorReview/?PId=${rowData.id}&Title=${rowData.title}&Publication=${rowData.kicdNumber}&Stage=PaymentVerification" class="btn btn-w-m btn-info" style="background-color:#00B95F;" role="button">Review</a>`));
+        row.append($(`<td> <a href="/PrincipalCurator/PrincipalCuratorReview/?Identity=${rowData.id}&Title=${rowData.title}&Publication=${rowData.kicdNumber}&Stage=PaymentVerification" class="btn btn-w-m btn-info" style="background-color:#00B95F;" role="button">Review</a>`));
 
 
         return row[0];
