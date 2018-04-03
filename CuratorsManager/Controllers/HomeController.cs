e8de608ea30da3ea9ab335e5e12f4d0a3e9be7ba
Makena Riungu <makena@miyabiafrica.com>
2018-04-03 16:49:22 +0000

    Curator manager UI Upload

---
+++ CuratorsManager/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using curatorsManager.Models;
+
+namespace curatorsManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Curators()
+        {
+                      return View();
+        }
+
+        public IActionResult ChiefCurators()
+        {
+           
+            return View();
+        }
+         public IActionResult PrincipalCurators()
+        {
+           
+            return View();
+        }
+         public IActionResult AllPublications()
+        {
+           
+            return View();
+        }
+         public IActionResult Approved()
+        {
+           
+            return View();
+        }
+         public IActionResult PartiallyApproved()
+        {
+           
+            return View();
+        }
+         public IActionResult Rejected()
+        {
+           
+            return View();
+        }
+
+
+
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
