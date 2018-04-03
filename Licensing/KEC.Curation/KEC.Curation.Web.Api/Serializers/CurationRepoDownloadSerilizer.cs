8d0eb975ebeeaba9d64c8e76e993b93af5207f79
Collins Omwenga <comwenga@live.com>
2018-04-03 17:58:32 +0300

    _assignment.Publication.Url;

--- Licensing/KEC.Curation/KEC.Curation.Web.Api/Serializers/CurationRepoDownloadSerilizer.cs
+++ Licensing/KEC.Curation/KEC.Curation.Web.Api/Serializers/CurationRepoDownloadSerilizer.cs
@@ -51,16 +51,18 @@ namespace KEC.Curation.Web.Api.Serializers
         {
             get
             {
-                var publicationId = _uow.PublicationSectionRepository.Get(_assignment.PublicationId)?.PublicationId;
-                var publication = _uow.PublicationRepository.Get(publicationId.GetValueOrDefault());
-                if (publication == null)
-                {
-                    return string.Empty;
-                }
-                else
-                {
-                    return publication.Url;
-                }
+                //var publicationId = _uow.PublicationSectionRepository.Get(_assignment.PublicationId)?.PublicationId;
+                //var publication = _uow.PublicationRepository.Get(publicationId.GetValueOrDefault());
+                //if (publication == null)
+                //{
+                //    return string.Empty;
+                //}
+                //else
+                //{
+                //    return publication.Url;
+                return _assignment.Publication.Url;
+
+                //}
             }
         }
     }
