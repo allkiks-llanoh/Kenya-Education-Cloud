c9783917cdf187d5d1f9b83661f9164ee4c661a3
Allan Koech <allan@miyabiafrica.com>
2018-04-24 13:46:21 +0300

    Custom Authorize Curation Mangers

--- Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/Controllers/CurationManagersController.cs
+++ Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/Controllers/CurationManagersController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KEC.Curation.UI.ActionFilters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,6 +7,9 @@ using System.Web.Mvc;
 
 namespace KEC.Curation.UI.Controllers
 {
+    [CustomAuthorize(Roles = "Curation Manager")]
+    [UserGuidJson]
+    [AllowCrossSiteJson]
     public class CurationManagersController : Controller
     {
         // GET: CurationManagers
