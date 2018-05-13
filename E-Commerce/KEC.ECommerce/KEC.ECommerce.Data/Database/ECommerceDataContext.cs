07c1e5a8874c10b2c8da866116ad43eff0d31b5d
Juma Daniel <juma@miyabiafrica.com>
2018-05-13 19:36:14 +0300

    E-Commerce Initial

---
+++ E-Commerce/KEC.ECommerce/KEC.ECommerce.Data/Database/ECommerceDataContext.cs
@@ -0,0 +1,26 @@
+﻿using KEC.ECommerce.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KEC.ECommerce.Data.Database
+{
+    public class ECommerceDataContext : DbContext
+    {
+        public ECommerceDataContext(DbContextOptions options) :
+            base(options)
+        {
+
+        }
+        public DbSet<Publication> Publications { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Level> Levels { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<LineItem> LineItems { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+    }
+}
