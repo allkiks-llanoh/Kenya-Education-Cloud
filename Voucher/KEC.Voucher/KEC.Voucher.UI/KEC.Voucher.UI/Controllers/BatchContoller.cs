84f78b70e1e93a41c049c634e20329deeadaed63
Makena Riungu <makena@miyabiafrica.com>
2018-05-17 15:56:29 +0000

    Adding voucher controller and Client/School UI Design

---
+++ Voucher/KEC.Voucher/KEC.Voucher.UI/KEC.Voucher.UI/Controllers/BatchContoller.cs
@@ -0,0 +1,40 @@
+﻿using KEC.Voucher.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KEC.Voucher.UI.Controllers
+{
+    public class BatchContoller : Controller
+    {
+        public ActionResult Index()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var user = context.Users.FirstOrDefault(u => u.Email.Equals(User.Identity.Name));
+                var voucherUser = new VoucherUser
+                {
+                    Guid = user.Id,
+                    FullName = user.FullName
+                };
+                return View(voucherUser);
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
