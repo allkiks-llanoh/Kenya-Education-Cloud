8400350f14e48a49780705262c3f982afc180a7b
Collins Omwenga <comwenga@live.com>
2018-05-20 19:00:34 +0300

    Added Ecommerce UI

---
+++ E-Commerce/KEC.ECommerce/KEC.Ecommerce.Web.UI/KEC.Ecommerce.Web.UI/Controllers/StoreController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KEC.Ecommerce.Web.UI.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class StoreController : Controller
+    {
+        public ActionResult ProductsList()
+        {
+            return View();
+        }
+
+        public ActionResult ProductsGrid()
+        {
+            return View();
+        }
+
+        public ActionResult ProductDetail(int id)
+        {
+            return View();
+        }
+
+        public ActionResult ProductEdit()
+        {
+            return View();
+        }
+
+        public ActionResult Payments()
+        {
+            return View();
+        }
+
+        public ActionResult Orders()
+        {
+            return View();
+        }
+
+        public ActionResult Cart()
+        {
+            return View();
+        }
+
+    }
+}
