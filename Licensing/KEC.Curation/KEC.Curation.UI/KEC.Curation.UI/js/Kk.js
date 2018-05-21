b2e770532cccaf91e18f334b4626f161497c90d9
Allan Koech <allan@miyabiafrica.com>
2018-05-21 22:51:19 +0300

    Publication Id and comments

---
+++ Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/js/Kk.js
@@ -0,0 +1,33 @@
+﻿
+$(document).ready(function () {
+
+    let urldrd = apiBaseUrl.concat(`/chiefcurator/publication/${publicationId}/curatorsubmissions?chiefCuratorGuid=${chiefCuratorGUID}&publicationId=${publicationId}`);
+
+    function tableRows(data) {
+        var tableRows = [];
+        for (var i = 0; i < data.length; i++) {
+            tableRows.push(drawRow(data[i]));
+        }
+        return tableRows;
+    };
+
+    //Start by getting publication list based on Payment Verification Stage
+    $.ajax({
+        url: urldrd,
+        type: "GET",
+        dataType: "json",
+        success: function (data, status, jqhxr) {
+            console.log(data);
+
+            //This code snipet prepares to append Json Data
+            $('#FinanceList').append(tableRows(data));
+        }
+    });
+
+    //This functionpopulates the tbody inner HTML with json data on call
+    function drawRow(rowData) {
+        var row = $("<tr />")
+        row.append($("<td>" + rowData.notes + "</td>"));
+        return row[0];
+    }
+});
