da3414d7f299847fabe4e855c1526bfa3aae4a42
Makena Riungu <makena@miyabiafrica.com>
2018-05-22 21:13:40 +0000

    Register huh!

--- Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/Models/IdentityModels.cs
+++ Licensing/KEC.Curation/KEC.Curation.UI/KEC.Curation.UI/Models/IdentityModels.cs
@@ -14,6 +14,7 @@ namespace KEC.Curation.UI.Models
         public string FullName { get; set; }
         public string Subject { get; set; }
         public int SubjectId{ get; set; }
+        public string RoleName { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
