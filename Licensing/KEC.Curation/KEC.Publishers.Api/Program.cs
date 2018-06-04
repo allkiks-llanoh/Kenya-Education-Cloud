cf89400be86debf66421adaa555f0a29e832e369
Collins Omwenga <comwenga@live.com>
2018-06-04 16:45:26 +0000

    Merged PR 64: Publisher's API add

--- Licensing/KEC.Curation/KEC.Publishers.Api/Program.cs
+++ Licensing/KEC.Curation/KEC.Publishers.Api/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Kec.Publishers.Web.Api
+namespace KEC.Publishers.Api
 {
     public class Program
     {
