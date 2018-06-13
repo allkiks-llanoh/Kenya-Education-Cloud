87429737a79a6a1a6f34a2c5a7fc75fe988fbeb0
Allan Koech <allan.kkoech@gmail.com>
2018-06-13 18:22:14 +0300

    Index

--- Voucher/KEC.Voucher/KEC.Voucher.Web.UI/voucher/voucherarea/approve_vouchers/manage/Index.php
+++ Voucher/KEC.Voucher/KEC.Voucher.Web.UI/voucher/voucherarea/approve_vouchers/manage/Index.php
@@ -173,81 +173,153 @@
     <!-- Page-Level Scripts -->
     
 </body>
+
+<script>
+        $(document).ready(function() {
+
+            $('.footable').footable();
+            $('.footable').footable();
+
+        });
+
+    </script>
+
 <script type="text/JavaScript">
-    function tableRows(data) {
+$(document).ready(function(){
+        var batchId     =  $('#batchId').attr('data-batch');
+        $.ajax({
+        url: `https://voucherapi.kec.ac.ke/api/vouchers/created?batchId=${batchId}`,
+        type: "GET",
+        headers : {
+                  'Accept' : 'application/json',
+                  'Content-Type' : 'application/json',
+                  'Access-Control-Expose-Headers':'*'
+              },
+        dataType: "json",    
+        success: function(data,textStatus,jqXHR) {
+            console.log(jqXHR.getResponseHeader('page-Headers'));
+            //This code snipet prepares to append Json Data
+            $("#createdvouchers").append(tableRows(data)); 
+        },
+        complete: function(jqXHR, textStatus) {
+            console.log(jqXHR.getResponseHeader('page-Headers'));
+      }
+    });   
+});
+</script>
+<!--  script to load counties stops here  -->
+
+   <!--SCripts Start Here--> 
+   <script type="text/JavaScript">
+//Start by getting voucher list based on batch Id
+
+//This functionpopulates the tbody inner HTML with json data on call
+function tableRows(data) {
         var tableRows= [];    
         for (var i = 0; i < data.length; i++) {
             tableRows.push(drawRow(data[i]));
         }
         return tableRows;
-        };
-        //Start by getting voucher list based on batch Id
-       
-        reloadSchoolTypes();
-      
-
-        
-//This functionpopulates the tbody inner HTML with json data on call
+    }
     function drawRow(rowData) {
-       
         var row = $("<tr />")
-        row.append($('<td class="hidden"> + rowData.Id + </td>'));
-        row.append($("<td>" + rowData.BatchNumber + "</td>"));
-        row.append($("<td>" + rowData.CountyCode + "</td>"));
-        row.append($("<td>" + rowData.CountyName + "</td>"));
-        row.append($("<td>" + rowData.SchoolType + "</td>"));
-        row.append($(`<td class="pull-right"> <button type="button" data-vid=${rowData.Id} class="btn btn-w-m btn-info btn-md NewBatch" role="button">CREATE VOUCHERS</button>`));  
+        row.append($("<td class='hidden'>" + rowData.Id + "</td>"));
+        row.append($("<td>" + rowData.SchoolName + "</td>"));
+    
+        row.append($("<td>" + rowData.WalletAmount + "</td>"));
+        row.append($(`<td> <input type='checkbox' id=${rowData.Id} data-voucher=${rowData.Id} name=${rowData.Id} 'approve' class='approve'/> </td>`));  
+        row.append($(`<td> <input type='checkbox' id=${rowData.Id} data-voucher=${rowData.Id} name=${rowData.Id} 'reject' class='reject'/> </td>`)); 
         return row[0];
        }
-       function reloadSchoolTypes(){
-        var d = new Date();
-       var n = d.getFullYear();
-        $.ajax({
-            url:`https://voucherapi.kec.ac.ke/api/batches/${n}/withpendingvouchers`,
-            type: "GET",
-            dataType: "json",   
-            success: function(data,status,jqhxr){
-                console.log(data);
-              
-                //This code snipet prepares to append Json Data
-                $('#createdvouchers>tbody').html(tableRows(data)); 
-                hookCreateBatchButtons(); 
-            },
-             
+   //This function Sends selected rows for rejection then replaces existeng rows with new rows 
+   function ajaxData(){
+        var BatchId     =  $('#batchId').attr('data-batch');
+        var voucherIdArray = []
+        var checked = $('.reject:checked');
+        $('.reject:checked').each((index,row)=>{
+            voucherIdArray.push(row['id'])
         });
-       }
-       function hookCreateBatchButtons(){
-           $('.NewBatch').click(function()
-          {
-            $(this).html('<i class="fa fa-refresh fa-spin"></i> Please wait');
-      
-            var bId        =  $(this).attr('data-vid');
-          
-            console.log($(this).attr('data-county'));
+        return JSON.stringify({BatchId: BatchId,SelectedVouchers: voucherIdArray })
+        };
+        $('#reject').click(function(e) {
+            e.preventDefault();
+            console.log(ajaxData());
             $.ajax({
-              headers : {
-                  'Accept' : 'application/json',
-                  'Content-Type' : 'application/json'
-              } ,
-          beforeSend: function(){
-              if(!$('div.alert-success').attr('class').includes('hidden')){
-                $('div.alert-success').toggleClass('hidden');
-              }
-          },
-          url: "https://voucherapi.kec.ac.ke/api/Vouchers",
-          type: "POST",
-          data: JSON.stringify({BatchId: bId}),
-          success: function(response,status,jxhr) {
-             console.log(response);
-             console.log(status);
-             $('#alert').html(`${response}`)
-             $('div.alert-success').toggleClass('hidden');
-             reloadSchoolTypes();
-            
-             }
-          });
-          });
-        }
+            headers : {
+                'Accept' : 'application/json',
+                'Content-Type' : 'application/json',
+                'Access-Control-Allow-Origin': 'https://voucherapi.kec.ac.ke/api/'
+            } ,
+        url: "https://voucherapi.kec.ac.ke/api/Vouchers/selected/reject",
+        type: "PATCH",
+        data: ajaxData() ,
+        success: function(data,status,jxhr) {
+             var remainingRows = tableRows(data);
+             var row = $("<tr/>");
+             row.append("<th class='hidden'> ORDER </th>")
+             row.append("<th> SCHOOL NAME </th>");
+             row.append("<th> VOUCHER AMOUNT</th>");
+             row.append("<th> APPROVE</th>");
+             row.append("<th> REJECT</th>");
+             remainingRows.unshift(row);
+            $('#createdvouchers>tbody').html(remainingRows); 
+           }
+        });
+    });
 </script> 
+<!--This Script Checks the rejection checkbox-->
+
+<!--HAPPY CODDING TO YOU LAZZY TESTERS-->
+
+
+             <!--SCripts Start Here--> 
+
+<!--Start by getting voucher list based on batch Id-->
+<script type="text/JavaScript">
+    function ajaxDatas(){
+        var BatchId     =  $('#batchId').attr('data-batch');
+        var voucherIdArray = []
+        var checked = $('.approve:checked');
+        $('.approve:checked').each((index,row)=>{
+            voucherIdArray.push(row['id'])
+        });
+        return JSON.stringify({BatchId: BatchId,SelectedVouchers: voucherIdArray })
+        };
+        $('#approve').click(function(e) {
+            e.preventDefault();
+            console.log(ajaxDatas());
+            $.ajax({
+            headers : {
+                'Accept' : 'application/json',
+                'Content-Type' : 'application/json',
+                'Access-Control-Allow-Origin': 'https://voucherapi.kec.ac.ke/api/'
+            } ,
+        url: "https://voucherapi.kec.ac.ke/api/vouchers/selected/accept",
+        type: "PATCH",
+        data: ajaxDatas() ,
+        success: function(data,status,jxhr) {
+             var remainingRows = tableRows(data);
+             var row = $("<tr/>");
+             row.append("<th> ORDER </th>")
+             row.append("<th> SCHOOL NAME </th>");
+             row.append("<th> VOUCHER AMOUNT</th>");
+             row.append("<th> APPROVE</th>");
+             row.append("<th> REJECT</th>");
+             remainingRows.unshift(row);
+            $('#createdvouchers>tbody').html(remainingRows); 
+           }
+        });
+    });  
+</script>
+<!--This Script Checks the Approve checkbox-->
+
+<script type="text/JavaScript">
+      $(function() {
+        $('.chk_approve').click(function() {
+        $('.approve').prop('checked', this.checked);
+      });
+  });
+</script> 
+<!--HAPPY CODDING TO YOU LAZZY TESTER-->
 
 </html>
