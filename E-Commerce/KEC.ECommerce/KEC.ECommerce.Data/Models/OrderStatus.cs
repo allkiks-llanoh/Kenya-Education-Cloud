3e552861d9bc76a00b2af905e6b1e73dc28ec7a9
Juma Daniel <juma@miyabiafrica.com>
2018-06-26 23:24:48 +0300

    Remove E-Commerce

--- E-Commerce/KEC.ECommerce/KEC.ECommerce.Data/Models/OrderStatus.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KEC.ECommerce.Data.Models
-{
-    public enum OrderStatus
-    {
-        Submitted=1,
-        Paid=2,
-        Processed = 3
-    }
-}
--- E-Commerce/KEC.ECommerce/KEC.ECommerce.Web.UI/Pagination/IPageConfig.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace KEC.ECommerce.Web.UI.Pagination
-{
-    public interface IPageConfig
-    {
-        int PageSize { get; }
-    }
-}
