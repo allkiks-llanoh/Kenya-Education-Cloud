bda9723e68d06bf8095a43f24acd223114a3169f
Juma Daniel <juma@miyabiafrica.com>
2018-06-26 23:38:55 +0300

    Security folder

---
+++ E-Commerce/KEC.ECommerce/KEC.Ecommerce.Web.UI/Security/ViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KEC.ECommerce.Web.UI.Security.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress, MaxLength(500)]
+        [Display(Name = "Email Address:")]
+        public string EmailAddress { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name ="Login Password:")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me:")]
+        public bool RememberMe { get; set; }
+    }
+}
