4370fd7522557a320ebb9c7b95f0ca1619478c31
Juma Daniel <juma@miyabiafrica.com>
2018-07-14 21:52:14 +0300

    Error pages

---
+++ E-Commerce/KEC.ECommerce/KEC.ECommerce.Web.UI/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KEC.ECommerce.Web.UI.Controllers
+{
+    [Route("error")]
+    public class ErrorController : Controller
+    {
+        [Route("500")]
+        public IActionResult AppError()
+        {
+            return View();
+        }
+        [Route("404")]
+        public IActionResult PageNotFound()
+        {
+            return View();
+        }
+    }
+}
--- E-Commerce/KEC.ECommerce/KEC.Ecommerce.Web.UI/Startup.cs
+++ E-Commerce/KEC.ECommerce/KEC.Ecommerce.Web.UI/Startup.cs
@@ -101,8 +101,21 @@ namespace KEC.ECommerce.Web.UI
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/error/500");
             }
+            app.Use(async (ctx, next) =>
+            {
+                await next();
+
+                if (ctx.Response.StatusCode == 404 && !ctx.Response.HasStarted)
+                {
+                    //Re-execute the request so the user gets the error page
+                    string originalPath = ctx.Request.Path.Value;
+                    ctx.Items["originalPath"] = originalPath;
+                    ctx.Request.Path = "/error/404";
+                    await next();
+                }
+            });
             app.UseForwardedHeaders();
             app.UseHangfireServer();
             app.UseAuthentication();
