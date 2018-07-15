d58fc8f7e7ee9abd1ef921e6fb4bdc8df7607790
Juma Daniel <juma@miyabiafrica.com>
2018-07-15 14:46:14 +0300

    Security

---
+++ E-Commerce/KEC.ECommerce/KEC.ECommerce.Web.UI/Security/Database/IdentityDataContext.cs
@@ -0,0 +1,20 @@
+﻿using KEC.ECommerce.Web.UI.Security.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace KEC.ECommerce.Web.UI.Security.Database
+{
+    public class IdentityDataContext : IdentityDbContext<ApplicationUser> 
+    {
+
+        public IdentityDataContext(DbContextOptions<IdentityDataContext> options) : base(options)
+        {
+       
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<ApplicationUser>().HasAlternateKey(p => p.IdentificationCode);
+        }
+    }
+}
---
+++ E-Commerce/KEC.ECommerce/KEC.ECommerce.Web.UI/Security/ViewModels/ForgotPasswordViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KEC.ECommerce.Web.UI.Security.ViewModels
+{
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+    }
+}
