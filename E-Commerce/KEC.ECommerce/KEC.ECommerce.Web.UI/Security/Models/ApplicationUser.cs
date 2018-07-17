e34a583fe3e42544017e5e524da8e92f20272610
Juma Daniel <juma@miyabiafrica.com>
2018-07-18 00:01:33 +0300

    Security files

---
+++ E-Commerce/KEC.ECommerce/KEC.ECommerce.Web.UI/Security/Models/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace KEC.ECommerce.Web.UI.Security.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string IdentificationCode { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DisplayName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+    }
+}
