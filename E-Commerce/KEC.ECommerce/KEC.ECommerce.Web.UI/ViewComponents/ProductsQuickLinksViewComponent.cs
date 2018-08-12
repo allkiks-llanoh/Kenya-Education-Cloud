87850e9e4787ae9c2386d6581b952fc668e0aefa
Juma Daniel <juma@miyabiafrica.com>
2018-08-12 23:15:40 +0300

    Products quick links

--- E-Commerce/KEC.ECommerce/KEC.ECommerce.Web.UI/ViewComponents/ProductsQuickLinksViewComponent.cs
+++ E-Commerce/KEC.ECommerce/KEC.ECommerce.Web.UI/ViewComponents/ProductsQuickLinksViewComponent.cs
@@ -6,19 +6,19 @@ using System.Linq;
 
 namespace KEC.ECommerce.Web.UI.ViewComponents
 {
-    public class ProductsMenuViewComponent: ViewComponent
+    public class ProductsQuickLinksViewComponent: ViewComponent
     {
         private readonly IUnitOfWork _uow;
 
-        public ProductsMenuViewComponent(IUnitOfWork uow)
+        public ProductsQuickLinksViewComponent(IUnitOfWork uow)
         {
             _uow = uow;
         }
         public IViewComponentResult Invoke()
         {
             var categories = _uow.CategoriesRepository.GetAll();
-            var categoryList = categories.Any() ? categories.Select(p => new CategoryViewModel(_uow,p)) : new List<CategoryViewModel>();
-            return View(categoryList?.OrderBy(p=> p.Name));
+            var categoryList = categories.Any() ? categories.Select(p => new CategoryViewModel(_uow, p)) : new List<CategoryViewModel>();
+            return View(categoryList?.OrderBy(p => p.Name));
         }
     }
 }
